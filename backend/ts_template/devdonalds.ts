0efaf3aa329e8648f11557bfdefbff51e6a32621
Rosemary Ai <rosemaryai.rtf@gmail.com>
2025-02-13 00:20:42 +1100

    i cooked

--- backend/ts_template/devdonalds.ts
+++ backend/ts_template/devdonalds.ts
@@ -19,6 +19,12 @@ interface ingredient extends cookbookEntry {
   cookTime: number;
 }
 
+interface summary {
+  name: string;
+  cookTime: number;
+  ingredients: requiredItem[];
+}
+
 // =============================================================================
 // ==== HTTP Endpoint Stubs ====================================================
 // =============================================================================
@@ -26,7 +32,7 @@ const app = express();
 app.use(express.json());
 
 // Store your recipes here!
-const cookbook: any = null;
+const cookbook: cookbookEntry[] = [];
 
 // Task 1 helper (don't touch)
 app.post("/parse", (req:Request, res:Response) => {
@@ -45,25 +51,127 @@ app.post("/parse", (req:Request, res:Response) => {
 // [TASK 1] ====================================================================
 // Takes in a recipeName and returns it in a form that 
 const parse_handwriting = (recipeName: string): string | null => {
-  // TODO: implement me
   return recipeName
+          .replace(/[-_]+/g, ' ')
+          .replace(/[^A-Za-z ]+/g, '')
+          .toLowerCase()
+          .replace(/\b\w/g, (c) => c.toUpperCase())
+          .trim() || null;
 }
 
 // [TASK 2] ====================================================================
 // Endpoint that adds a CookbookEntry to your magical cookbook
 app.post("/entry", (req:Request, res:Response) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
+  const entry = req.body;
 
+  try {
+    validateEntry(entry);
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+    return;
+  }
+
+  res.json(addEntry(entry));
+  return;
 });
+
+// Validates a cookbook entry
+const validateEntry = (entry: cookbookEntry) => {
+  if (cookbook.some((e) => e.name === entry.name)) throw new Error("skill issue");
+
+  if (entry.type === "recipe") {
+    const recipe = entry as recipe;
+    if (hasDuplicates(recipe.requiredItems)) {
+      throw new Error("skill issue");
+    }
+  } else if (entry.type === "ingredient") {
+    const ingredient = entry as ingredient;
+    if (ingredient.cookTime < 0) {
+      throw new Error("skill issue");
+    }
+  } else {
+    throw new Error("skill issue");
+  }
+}
+
+// Returns whether or not an array contains duplicate ingredients
+const hasDuplicates = (items: requiredItem[]): boolean => {
+  const names = new Map();
+  for (const i of items) {
+    if (names.has(i.name)) {
+      return true;
+    }
+    names.set(i.name, 42);
+  }
+  return false;
+}
+
+// Adds an entry to the cookbook
+const addEntry = (entry: cookbookEntry) => {
+  cookbook.push(entry);
+  return {};
+}
 
 // [TASK 3] ====================================================================
 // Endpoint that returns a summary of a recipe that corresponds to a query name
 app.get("/summary", (req:Request, res:Request) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
+  const name = req.query.name as string;
 
+  try {
+    validateName(name);
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+    return;
+  }
+
+  res.json(getSummary(name));
+  return;
 });
+
+// Validates the name of a recipe
+const validateName = (name: string) => {
+  const entry = cookbook.find((e) => e.name === name);
+  if (!entry || entry.type !== "recipe") throw new Error("skill issue");
+  
+  const recipe = entry as recipe;
+  for (const item of recipe.requiredItems) {
+    if (!cookbook.some((e) => e.name === item.name)) {
+      throw new Error("skill issue");
+    }
+  }
+}
+
+// Returns the summary of a recipe
+const getSummary = (name: string): summary => {
+  const recipe = cookbook.find((e) => e.name === name) as recipe;
+  const summary = {
+    name: name,
+    cookTime: 0,
+    ingredients: [] as requiredItem[],
+  }
+
+  addIngredientsToSummary(recipe, summary);
+
+  return summary;
+}
+
+// Adds ingredients of a recipe to a recipe summary
+const addIngredientsToSummary = (recipe: recipe, summary: summary) => {
+  for (const item of recipe.requiredItems) {
+    const entry = cookbook.find((e) => e.name === item.name);
+    if (entry.type === "recipe") {
+      addIngredientsToSummary(entry as recipe, summary);
+    } else {
+      const ingredient = summary.ingredients.find((i) => i.name === entry.name);
+      if (ingredient) {
+        ingredient.quantity += item.quantity;
+      } else {
+        summary.ingredients.push(item);
+      }
+      summary.cookTime += (entry as ingredient).cookTime * item.quantity;
+    }
+  }
+}
 
 // =============================================================================
 // ==== DO NOT TOUCH ===========================================================
